a2f9377289fae9b19c3197b122419cfe6bf52877
arunkumargajengi <arunkumar.gajengi@gmail.com>
2024-11-20 17:24:11 +0530

    added cities to the ArrayList code

---
+++ AKJavafullstatckDemo/src/com/velocity/ArrayListDemo1.java
@@ -0,0 +1,16 @@
+package com.velocity;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ArrayListDemo1 {
+public static void main(String[]args) {
+	List<String> list = new ArrayList<String>();
+	// add elements
+	list.add("pune");
+	list.add("mubai");
+	list.add("goa");
+	list.add("hyderabad");
+	System.out.println(list);
+}
+}
