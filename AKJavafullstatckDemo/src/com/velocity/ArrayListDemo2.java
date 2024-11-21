fee7fbd7a3e131a469dbe7f6edfe4618a0fff7fc
arunkumargajengi <arunkumar.gajengi@gmail.com>
2024-11-21 15:33:42 +0530

    integer values get addeed in code

--- AKJavafullstatckDemo/src/com/velocity/ArrayListDemo2.java
+++ AKJavafullstatckDemo/src/com/velocity/ArrayListDemo2.java
@@ -2,6 +2,9 @@ package com.velocity;
 
 public class ArrayListDemo2 {
 	public static void main(String[]args) {
+		int a=10;
+		int b=20;
+		int c=30;
 System.out.println("hello im in arraylist demo 2");
 	
 }
